209a156a038e42b5d8ed91097ad79ca882fe7f32
walmsles <2704782+walmsles@users.noreply.github.com>
2023-02-07 14:13:09 +1100

    amplify demo additions - pre

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  SocialPostCollection 
+ } from './ui-components';
+ import { Amplify } from 'aws-amplify';
 
-function App() {
+ import { withAuthenticator } from '@aws-amplify/ui-react';
+ import '@aws-amplify/ui-react/styles.css';
+
+function App({signOut, user}) {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello {user.attributes.name}</h1>
+
+      <SocialPostCollection />
+
+      <button onClick={signOut}>Sign out</button>
     </div>
   );
 }
 
 export default App;
+// export default withAuthenticator(App);
